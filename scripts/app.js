10621e25207e2fac4f67209db73016c3b4fa9fee
Jiali <keepwarm520@gmail.com>
2018-07-10 17:11:34 +0800

    first day commit
    1st revision

---
+++ scripts/app.js
@@ -0,0 +1,37 @@
+//创建app应用模块
+var yike = angular.module('yike',[]);
+/*
+run方法在模块创建好之后会直接执行
+ */
+yike.run(["$rootScope",function($rootScope){
+//给头部a标签绑定单击事件:点击实现侧边导航栏的显示或隐藏
+//给collapse类定义值,通过这个值决定collapse是否有效
+	$rootScope.collapsed = false;
+	$rootScope.toggle=function(){
+		$rootScope.collapsed = !$rootScope.collapsed;
+		//显示隐藏导航栏标题内容
+		//获取导航栏中所有的dd
+		var dds=document.querySelectorAll('dd');
+		//遍历每一个dd,给每个dd修改样式实现隐藏显示的效果
+		if ($rootScope.collapsed) {   //此时显示
+			for (var i =0; i<dds.length;i++) {
+		  		dds[i].style.transitionDuration = (i + 1) * 0.15 + 's';
+				dds[i].style.transitionProperty = 'all';
+				dds[i].style.transitionDelay = '0.2s';
+				dds[i].style.transitionTimingFunction = 'ease-out';
+				dds[i].style.transform = 'translate(0)';
+		  }
+		}else{
+			for (var i = dds.length - 1; i >= 0; i--) {
+		
+			
+				dds[i].style.transitionDuration = (dds.length - i + 1) * 0.05 + 's';
+				dds[i].style.transitionProperty = 'all';
+				dds[i].style.transitionDelay = '';
+				dds[i].style.transitionTimingFunction = 'ease-out';
+				dds[i].style.transform = 'translate(-100%)';
+			}
+
+		}
+	}
+}]);
